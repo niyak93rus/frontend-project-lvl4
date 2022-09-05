5c267eff3d434f93e3f796532fd7eedc0eca7ade
Nikolay_Yakovlev <n.yakovlev96@gmail.com>
2022-09-05 07:48:33 +0500

    update: update 404 page

--- frontend/src/components/NoMatch.jsx
+++ frontend/src/components/NoMatch.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
+import notFound from '../assets/notFound.png';
+
 const NoMatch = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1>{t('pageNotFound')}</h1>
-      <Link to="/">{t('backToMain')}</Link>
+    <div className="d-flex flex-column ">
+      <h1 className="text-center">{t('pageNotFound')}</h1>
+      <div className="text-center">
+        <img src={notFound} alt="404" width={400} />
+      </div>
+      <div className="row mt-2 p-2 text-center bg-light align-items-stretch">
+        <Link to="/">{t('backToMain')}</Link>
+      </div>
     </div>
   );
 };
